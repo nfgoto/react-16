9fa3d58097a09be40edd360274965514680749c6
goto florian <gotoflorian.pro@gmail.com>
2018-02-28 16:56:10 +0100

    add props validation

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Person.css'
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
@@ -38,6 +40,13 @@ class Person extends Component {
         );
     }
 
+}
+
+Person.propTypes = {
+    clicked: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);
