b255cbf2ef3bb39967344bbfeb2dbf9cd7abc1c5
goto florian <gotoflorian.pro@gmail.com>
2018-02-28 12:59:37 +0100

    refactor WithClass hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.css'
-import WithClass from '../../../hoc/WithClass';
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
 
 
 class Person extends Component {
@@ -21,7 +22,7 @@ class Person extends Component {
     render() {
         console.log('[Person.js] Inside render()');
         return (
-            <WithClass classes={classes.Person}>
+            <Aux>
     
                 {/* can pass methods as property which might change state in another compo */}
                 {/* in JSX, event name use camelCase, unlike in HTML */}
@@ -33,10 +34,10 @@ class Person extends Component {
     
                 {/* onChange handler triggered when value in input changes */}
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </WithClass>
+            </Aux>
         );
     }
 
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 
 /* components names by convention srart with uppercase */
@@ -128,16 +129,16 @@ class App extends PureComponent {
 
     return (
       /* only one root element */
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => this.setState({showPersons: true}) }>Show People</button>
         <Cockpit  appTitle={this.props.appTitle}
                   persons={this.state.persons}
                   showPersons={this.state.showPersons}
                   clicked={this.togglePersonsHandler} />
         {persons}      
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App
+export default withClass(App, classes.App);
